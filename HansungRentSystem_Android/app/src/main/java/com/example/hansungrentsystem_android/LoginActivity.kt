8e3e2a321e43db4b91294108caf5141a47e01e63
yoongyu-hwang <dotheji11@naver.com>
2022-06-28 03:45:22 +0900

    로그인 구현

--- HansungRentSystem_Android/app/src/main/java/com/example/hansungrentsystem_android/LoginActivity.kt
+++ HansungRentSystem_Android/app/src/main/java/com/example/hansungrentsystem_android/LoginActivity.kt
@@ -2,11 +2,66 @@ package com.example.hansungrentsystem_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        val idField = findViewById<EditText>(R.id.idField)
+        val pwdField = findViewById<EditText>(R.id.pwdField)
+
+        loginBtn.setOnClickListener {
+            thread(start = true) {
+                try {
+                    var urlText = "http://10.0.2.2:8080/API/login?userId="+idField.text+"&password="+pwdField.text
+
+                    val url = URL(urlText)
+                    val netConn = url.openConnection() as HttpURLConnection
+
+                    if (netConn.responseCode == HttpURLConnection.HTTP_OK) {
+                        val streamReader = InputStreamReader(netConn.inputStream)
+                        val buffered = BufferedReader(streamReader)
+
+                        val content = StringBuilder()
+                        while (true) {
+                            val line = buffered.readLine() ?: break
+                            content.append(line)
+                        }
+                        buffered.close()
+                        netConn.disconnect()
+                        System.out.println(content.toString());
+
+
+
+                        val json = JSONObject(content.toString())
+                        val pwd =json["password"].toString()
+                        System.out.println(pwd);
+
+                    }
+
+                } catch (e: Exception) {
+                    System.out.println("로그인실패@@@@@@@@@@@@@@@@@@@@@@");
+
+                }
+
+            }
+
+        }
+
+
+
 
     }
 }
