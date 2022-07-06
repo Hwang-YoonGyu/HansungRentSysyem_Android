49a07dac41c57fffcbe347d0a7d691943bced11f
Hwang-YoonGyu <71763096+zack8361@users.noreply.github.com>
2022-07-06 21:00:50 +0900

    앱 종료 기능 + list ...
    수고링

--- HansungRentSystem_Android/app/src/main/java/com/example/hansungrentsystem_android/MainActivity.kt
+++ HansungRentSystem_Android/app/src/main/java/com/example/hansungrentsystem_android/MainActivity.kt
@@ -18,13 +18,20 @@ class MainActivity : AppCompatActivity() {
 
         System.out.println("메인에서 유저의 이름은 : "+user?.userName)
 
+
         val viewBtn = findViewById<Button>(R.id.viewBtn)
+        val terminateBtn = findViewById<Button>(R.id.terminateBtn)
         var userInfo = findViewById<TextView>(R.id.userInfo)
         userInfo.text = user.userId + " " +user.userName
             viewBtn.setOnClickListener {
                 val intent = Intent(this, AskActivity::class.java)
                 startActivity(intent)
             }
+            terminateBtn.setOnClickListener{
+                val intent = Intent(this,LoginActivity::class.java)
+                startActivity(intent)
+            }
+
         }
 
 
