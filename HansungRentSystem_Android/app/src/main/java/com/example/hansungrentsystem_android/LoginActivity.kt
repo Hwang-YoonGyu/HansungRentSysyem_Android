16c12966ff7119b2f9715f8809d8eec0b04608cd
jinwoongCho <100175892+jinwoongCho@users.noreply.github.com>
2022-07-20 20:45:05 +0900

    자동로그인 허술하게 완성

--- HansungRentSystem_Android/app/src/main/java/com/example/hansungrentsystem_android/LoginActivity.kt
+++ HansungRentSystem_Android/app/src/main/java/com/example/hansungrentsystem_android/LoginActivity.kt
@@ -6,15 +6,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.lang.Exception
+import java.io.*
 import java.lang.StringBuilder
 import java.lang.reflect.Type
 import java.net.HttpURLConnection
 import java.net.URL
+import kotlin.Exception
 import kotlin.concurrent.thread
 
 class LoginActivity : AppCompatActivity() {
@@ -26,63 +26,89 @@ class LoginActivity : AppCompatActivity() {
         val idField = findViewById<EditText>(R.id.idField)
         val pwdField = findViewById<EditText>(R.id.pwdField)
 
+        try {
+            var filePath = filesDir.path + "/myText.txt"
+
+            val file = File(filePath)
+            val fileReader = FileReader(file)
+            var woong = fileReader.readText()
+            if(woong.isEmpty()) {
+                println("No data")
+            }
+            else{
+                var temp = woong.split(" ")
+                doLogin(temp[0], temp[1])
+            }
+        }catch (e:Exception){
+            println("error")
+        }
+
 
         loginBtn.setOnClickListener {
-            this.runOnUiThread {
+            doLogin(idField.text.toString(), pwdField.text.toString())
+        }
+    }
 
-            }
-            thread(start = true) {
-                try {
-                    var urlText = "http://223.194.158.173:8080/API/login?userId="+idField.text+"&password="+pwdField.text
+    fun doLogin(id:String, pwd:String) {
+        this.runOnUiThread {
 
-                    val url = URL(urlText)
-                    val netConn = url.openConnection() as HttpURLConnection
+        }
+        thread(start = true) {
+            try {
+                val autoLogin = findViewById<CheckBox>(R.id.checkBox)
+                var urlText = "http://223.194.158.173:8080/API/login?userId="+id+"&password="+pwd
 
-                    if (netConn.responseCode == HttpURLConnection.HTTP_OK) {
-                        val streamReader = InputStreamReader(netConn.inputStream)
-                        val buffered = BufferedReader(streamReader)
+                val url = URL(urlText)
+                val netConn = url.openConnection() as HttpURLConnection
 
-                        val content = StringBuilder()
-                        while (true) {
-                            val line = buffered.readLine() ?: break
-                            content.append(line)
-                        }
-                        buffered.close()
-                        netConn.disconnect()
-                        System.out.println(content.toString());
+                if (netConn.responseCode == HttpURLConnection.HTTP_OK) {
+                    val streamReader = InputStreamReader(netConn.inputStream)
+                    val buffered = BufferedReader(streamReader)
 
-
-
-                        val json = JSONObject(content.toString())
-                        val pwd =json["password"].toString()
-                        var user = User.getInstance(this)
-                        user.setData(userId = json["id"].toString(), userName = json["userName"].toString(),Type= json["Type"].toString(), password = json["password"].toString(), userPhone = json["userPhone"].toString(), isRented = json["isRented"].toString())
+                    val content = StringBuilder()
+                    while (true) {
+                        val line = buffered.readLine() ?: break
+                        content.append(line)
+                    }
+                    buffered.close()
+                    netConn.disconnect()
+                    System.out.println(content.toString());
 
 
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.putExtra("userId",user.userId)
+                    val json = JSONObject(content.toString())
+                    val pwd =json["password"].toString()
+                    var user = User.getInstance(this)
+                    user.setData(userId = json["id"].toString(), userName = json["userName"].toString(),Type= json["Type"].toString(), password = json["password"].toString(), userPhone = json["userPhone"].toString(), isRented = json["isRented"].toString())
 
-                        startActivity(intent)
+                    if(autoLogin.isChecked){
+                        var filePath = filesDir.path + "/myText.txt"
+                        val file = File(filePath)
+                        val fileWriter = FileWriter(file, false)
+                        var loginInfo = id + " " + pwd
+
+                        fileWriter.write(loginInfo)
+                        fileWriter.close()
+
                     }
 
-                } catch (e: Exception) {
-                    this.runOnUiThread {
-                        System.out.println("오류 : "+e.toString());
-                        val builder = AlertDialog.Builder(this)
-                        builder.setTitle("").setMessage("입력하신 정보가 올바르지 않습니다.").setPositiveButton("확인", { _: DialogInterface, i:Int ->
-                        })
-                        val alertDialog: AlertDialog = builder.create()
-                        alertDialog.show()
-                    }
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("userId",user.userId)
 
+                    startActivity(intent)
+                }
+
+            } catch (e: Exception) {
+                this.runOnUiThread {
+                    System.out.println("오류 : "+e.toString());
+                    val builder = AlertDialog.Builder(this)
+                    builder.setTitle("").setMessage("입력하신 정보가 올바르지 않습니다.").setPositiveButton("확인", { _: DialogInterface, i:Int ->
+                    })
+                    val alertDialog: AlertDialog = builder.create()
+                    alertDialog.show()
                 }
 
             }
 
         }
-
-
-
-
     }
 }
