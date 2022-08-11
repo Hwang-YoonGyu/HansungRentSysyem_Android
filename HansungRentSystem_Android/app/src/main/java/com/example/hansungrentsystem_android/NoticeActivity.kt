4568ba1e7ea0571ee7b83fe8111ea44add4774bd
sunpungi0623 <71875287+sunpungi0623@users.noreply.github.com>
2022-08-11 21:01:45 +0900

    공지 최종

--- HansungRentSystem_Android/app/src/main/java/com/example/hansungrentsystem_android/NoticeActivity.kt
+++ HansungRentSystem_Android/app/src/main/java/com/example/hansungrentsystem_android/NoticeActivity.kt
@@ -7,22 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 
 class NoticeActivity : AppCompatActivity() {
 
-
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_notice)
+            setContentView(R.layout.activity_noti)
 
-
-            val back = findViewById<TextView>(R.id.Notice_backBtn)
+            val back = findViewById<TextView>(R.id.Noti_backBtn)
             back.setOnClickListener {
                 onBackPressed()
             }
-
-
-
         }
-
-
 }
 
 
