8a895609d546e60e3a84f3376fa80b668fcca360
zack8361 <71763096+zack8361@users.noreply.github.com>
2022-08-18 20:08:35 +0900

    gradle
    gradle

---
+++ HansungRentSystem_Android/app/src/main/java/com/example/hansungrentsystem_android/MyFirebaseMessagingService.kt
@@ -0,0 +1,25 @@
+package com.example.hansungrentsystem_android
+
+
+import android.util.Log.d
+import com.google.android.datatransport.runtime.logging.Logging
+import com.google.android.datatransport.runtime.logging.Logging.d
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) { // Get updated InstanceID token.
+        Logging.d(TAG, "Refreshed token: $token")
+// TODO: Implement this method to send any registration to your app's servers.
+// sendRegistrationToServer(token)
+    }
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Logging.d(TAG, "From: ${remoteMessage.from}")
+        Logging.d(TAG, "Message data payload: ${remoteMessage.data}")
+        val msgBody = remoteMessage.notification?.body
+        Logging.d(TAG, "Message Notification Body: $msgBody")
+    }
+    companion object {
+        const val TAG = "MyFirebaseMessaging"
+    }
+}
